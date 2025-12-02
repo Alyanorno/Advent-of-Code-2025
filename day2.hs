72f73a0984f8a4af1c5bedbeda91a513f7035a22
Rickard Fridvall <rickard.fridvall@semcon.com>
2025-12-02 19:57:35 +0100

    Solved first part, not yet second

--- day2.hs
+++ day2.hs
@@ -4,9 +4,11 @@ import Data.List.Split
 main :: IO()
 main = do
 	c <- readFile "day2_input"
-	print . day1 $ c
+	print . day2 $ c
+	print . day2_2 $ c
 
-day1 = sum . concat . map (foo . map read . splitOn "-") . splitOn ","
+
+day2 = sum . concat . map (foo . map read . splitOn "-") . splitOn ","
 	where
 	foo :: [Integer] -> [Integer]
 	foo (x:y:[]) = baa x y
@@ -21,4 +23,26 @@ day1 = sum . concat . map (foo . map read . splitOn "-") . splitOn ","
 				s = (length a) `div` 2
 				a = show x
 				un_even n = n `mod` 2 /= 0
+
+
+day2_2 = concat . map (foo . map read . splitOn "-") . splitOn ","
+	where
+	foo :: [Int] -> [Int]
+	foo (x:y:[]) = baa x y
+		where
+		baa :: Int -> Int -> [Int]
+		baa start end
+			| start == end = []
+			| equal [chunksOf s a | s <- divisors (length a)] = read a : baa (start+1) end
+			| otherwise = baa (start+1) end
+			where
+				equal (l1:l2:ls)
+					| l1 == l2 = equal (l2:ls)
+					| otherwise = False
+				equal _ = True
+
+				a = show start
+
+				divisors :: Int -> [Int]
+				divisors n = [x | x <- [1.. (n `div` 2)], n `rem` x == 0]
 
