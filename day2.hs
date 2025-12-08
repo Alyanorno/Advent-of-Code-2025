345c0c586b06014e45a005730dfa791fcd7d7c7b
Alyanorno <partim@live.com>
2025-12-08 21:15:10 +0100

    Better day2

--- day2.hs
+++ day2.hs
@@ -1,15 +1,14 @@
 import Data.List.Split
+import Data.List
 
 
-day2 = sum . concatMap (filter (repeats . show) . (\[a,b] -> [a..b-1]) . map read . splitOn "-") . splitOn ","
+day2 = sum . concatMap (filter (repeats . show) . (\[a,b] -> [a..b]) . map read . splitOn "-") . splitOn ","
 	where
 	repeats s = even (length s) && ((\(a,b) -> a == b) (splitAt (length s `div` 2) s ))
 
-day2_2 = sum . concatMap (filter (repeats . show) . (\[a,b] -> [a..b-1]) . map read . splitOn "-") . splitOn ","
+day2_2 = sum . concatMap (filter (repeats . show) . (\[a,b] -> [a..b]) . map read . splitOn "-") . splitOn ","
 	where
-	repeats s = let l = length s in any check [k | k <- [1..l-1], l `mod` k == 0, l `div` k >= 2]
-		where
-		check k = all (== (take k s)) $ chunksOf k s
+	repeats s = let l = length s in any ((\(x:xs) -> all (== x) xs) . flip chunksOf s) [k | k <- [1..l], l `mod` k == 0, l `div` k >= 2]
 
 
 main = readFile "day2_input" >>= mapM_ print . sequence [day2, day2_2]
