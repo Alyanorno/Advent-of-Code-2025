101727d295bb3e914877e761b9a22e08c927198d
Alyanorno <partim@live.com>
2025-12-09 17:35:12 +0100

    Nicer Day4

--- day4.hs
+++ day4.hs
@@ -4,27 +4,13 @@ import Data.List
 
 day4 = length . removable . lines
 
-day4_2 = loop 0 . lines
-	where
-	loop count grid
-		| removed == 0 = count
-		| otherwise = loop (count + removed) grid'
-		where
-		r = removable grid
-		(grid', removed) = (update, length r)
-		h = length (head grid)
-		w = length grid
-		update = [[if (x,y) `elem` r then '.' else grid!!x!!y | x <- [0..w-1]] | y <- [0..h-1]]
+day4_2 = sum . map length . takeWhile (not . null) . map removable . iterate loop . lines
+	where loop g = remove (removable g)
+		where remove r = [[if (x,y) `elem` r then '.' else g!!x!!y | x <- [0..length g-1]] | y <- [0..length (head g)-1]]
 
-removable grid = [(x,y) | x <- [0..w-1], y <- [0..h-1], grid!!x!!y == '@', adj x y < 4]
-	where
-	h = length (head grid)
-	w = length grid
-
-	adj x y = sum [1 | ax <- [-1..1], ay <- [-1..1], ax /= 0 || ay /= 0, f (x+ax) (y+ay)]
-		where
-		f x y = maybe False (=='@') (grid!?x >>= (!?y))
+removable grid = [(x,y) | x <- [0..length (head grid)-1], y <- [0..length (head grid)-1], grid!!x!!y == '@', adj x y < 4]
+	where adj x y = sum [1 | ax <- [-1..1], ay <- [-1..1], (ax,ay) /= (0,0), f (x+ax) (y+ay)]
+		where f x y = maybe False (=='@') (grid!?x >>= (!?y))
 
 
 main = readFile "day4_input" >>= mapM_ print . sequence [day4, day4_2]
-
