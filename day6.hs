175c57261c38d1cd46c935d7d8c7f98e6caba5c6
Alyanorno <partim@live.com>
2025-12-10 16:40:29 +0100

    Solved day6

--- day6.hs
+++ day6.hs
@@ -1,8 +1,22 @@
+import Data.List
+import Data.Maybe (listToMaybe)
 
 
-day6 _ = ""
+day6 = sum . map (calc . reverse) . transpose . map words . lines
+	where
+	calc ("+":ls) = sum $ map read ls
+	calc ("*":ls) = product $ map read ls
 
-day6_2 _ = ""
+day6_2 =  sum . map calc . split . concatMap reverse . transpose . lines
+	where
+	calc ('+':ls) = sum $ readMany ls
+	calc ('*':ls) = product $ readMany ls
+
+	readMany = unfoldr $ listToMaybe . concatMap reads . tails . reverse
+
+	split (l:ls) = (l:x) : split ls'
+		where (x, ls') = span (not . (flip elem "+*")) ls
+	split [] = []
 
 
 main = mapM_ print . sequence [day6, day6_2] =<< readFile "day6_input"
