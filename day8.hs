65a8686a47b6e181de5524e657a81f8eff70db89
Alyanorno <partim@live.com>
2025-12-11 16:06:44 +0100

    Solved day8

--- day8.hs
+++ day8.hs
@@ -1,8 +1,35 @@
+import Data.List (sort, sortBy)
+import Data.List.Split
+import Data.Ord
 
 
-day8 _ = ""
+nrPairs = 1000
 
-day8_2 _ = ""
+day8 = product . take 3 . sortBy (comparing Down) . map length . foldl' merge (map (:[]) [0..nrPairs-1]) . take nrPairs . sort . calc . zip [0..] . map (map read . splitOn ",") . lines
+	where
+	calc indexed = [(distance x y, xi, yi) | (xi, x) <- indexed, (yi, y) <- indexed, xi < yi]
+	merge ls (_, xi, xy)
+		| x == y = ls
+		| otherwise = (x ++ y) : filter (`notElem` [x, y]) ls
+		where
+		x = find xi ls
+		y = find xy ls
+
+day8_2 = (\indexed -> step indexed start $ sort $ calc indexed) . zip [0..] . map (map read . splitOn ",") . lines
+	where
+	start = map (:[]) [0..nrPairs-1]
+	calc indexed = [(distance x y, xi, yi) | (xi, x) <- indexed, (yi, y) <- indexed, xi < yi]
+	step indexed ls ((_, i, j) : rest)
+		| x == y = step indexed ls rest
+		| length new == 1 = product $ map (head . snd . (indexed !!)) [i, j]
+		| otherwise = step indexed new rest
+		where
+		x = find i ls
+		y = find j ls
+		new = (x ++ y) : filter (`notElem` [x, y]) ls
+
+distance x y = sum $ map (^2) $ zipWith (-) x y
+find i = head . filter (elem i)
 
 
 main = mapM_ print . sequence [day8, day8_2] =<< readFile "day8_input"
