01d1035ee8107fc2f25eff6a79387678fed54bac
Alyanorno <partim@live.com>
2025-12-11 18:25:35 +0100

    Solved part 1 of day11

--- day11.hs
+++ day11.hs
@@ -1,8 +1,15 @@
+import Data.Maybe
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 
-day11 _ = ""
+day11 = traverse "you" . Map.fromList . map ((\(key:ls) -> (init key, ls)) . words) . lines
+	where
+	traverse node graph
+		| node == "out" = 1
+		| otherwise = sum $ map (flip traverse graph) $ fromJust $ Map.lookup node graph
 
-day11_2 _ = ""
+day11_2 _ = 0
 
 
 main = mapM_ print . sequence [day11, day11_2] =<< readFile "day11_input"
