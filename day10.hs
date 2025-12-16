ae0124b47a87830813db60e53da349a4c64e41b5
Alyanorno <partim@live.com>
2025-12-16 17:29:58 +0100

    Code for part 2 day10, but had to find the solution with a solver

--- day10.hs
+++ day10.hs
@@ -1,3 +1,5 @@
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.List
 import Data.List.Split
 import Data.Bits (xor, setBit)
@@ -14,7 +16,32 @@ day10 = sum . map minButtons . map (parse . words) . lines
 	minButtons :: (Int, [Int]) -> Int
 	minButtons (target, buttons) = minimum [length subset | subset <- subsequences buttons, (foldl' xor 0 subset) == target]
 
-day10_2 _ = 0
+day10_2 = sum . map minPresses . map parseMachine . lines
+	where
+	parseMachine line = (target, buttonVectors)
+		where
+		parts = words line
+		target = parseTarget $ last parts
+		buttons = map parseButton $ init $ tail parts
+		n = length target
+		buttonVectors = map (buttonToVector n) buttons
+
+	parseTarget s = map read $ splitOn "," $ init $ drop 1 s
+	parseButton "()" = []
+	parseButton s = map read $ splitOn "," $ init $ drop 1 s
+
+	buttonToVector n indices = [if i `elem` indices then 1 else 0 | i <- [0..n-1]]
+
+	minPresses :: ([Int], [[Int]]) -> Int
+	minPresses (target, buttons) = foo Set.empty [(0, replicate n 0)]
+		where
+		n = length target
+		foo visited [] = error "No solution"
+		foo visited ((presses, state):queue)
+			| state == target = presses
+			| state `Set.member` visited = foo visited queue
+			| otherwise = foo (Set.insert state visited) (queue ++ newItems)
+			where newItems = [(presses + 1, s) | btn <- buttons, let s = zipWith (+) state btn, all (uncurry (<=)) $ zip s target, s `Set.notMember` visited]
 
 
 main = mapM_ print . sequence [day10, day10_2] =<< readFile "day10_input"
