84d0f046d53e6d6def9092bfac5e0943e58bf1f8
Rickard Fridvall <rickard.fridvall@semcon.com>
2025-12-16 08:39:42 +0100

    Solved part 2 of day9... maybe

--- day9.hs
+++ day9.hs
@@ -1,11 +1,24 @@
 import Data.List.Split
 import Data.List
 import Data.Ord
+import qualified Data.Set as Set
 
 
 day9 = head . sortBy (comparing Down) . (\a -> [(abs (x1-y1)+1) * (abs (x2-y2)+1) | (x1:x2:[]) <- a, (y1:y2:[]) <- a]) . map (map read . splitOn ",") . lines
 
-day9_2 _ = 0
+day9_2 :: String -> Int
+day9_2 input = head $ map fst $ filter foo $ sortBy (comparing Down) [((abs (x1-y1)+1) * (abs (x2-y2)+1), ((x1,x2), (y1,y2))) | (x1:x2:[]) <- points, (y1:y2:[]) <- points, x1 /= x2 && y1 /= y2]
+	where
+	foo (_, ((x1,x2), (y1,y2))) = all (`Set.member` allowed) [[x,y] | x <- [min x1 x2..max x1 x2], y <- [min y1 y2..max y1 y2]]
+	points = map (map read . splitOn ",") $ lines input
+	(minX, maxX, minY, maxY) = (minimum $ map head points, maximum $ map head points, minimum $ map last points, maximum $ map last points)
+	greens = nub $ concat [line p1 p2 | (p1,p2) <- zip points (tail $ cycle points)] ++ [(x:y:[]) | x <- [minX..maxX], y <- [minY..maxY], inside points [x,y]]
+		where
+		inside poly (x:y:[]) = odd $ length $ filter (\((x1:y1:[]),(x2:y2:[])) -> y1 /= y2 && min y1 y2 < y && y <= max y1 y2 && x < x1) $ zip poly (tail $ cycle poly)
+		line (x1:y1:[]) (x2:y2:[])
+			| x1 == x2 = [[x1, y] | y <- [min y1 y2 .. max y1 y2]]
+			| y1 == y2 = [[x, y1] | x <- [min x1 x2 .. max x1 x2]]
+	allowed = Set.fromList $ nub $ points ++ greens
 
 
 main = mapM_ print . sequence [day9, day9_2] =<< readFile "day9_input"
